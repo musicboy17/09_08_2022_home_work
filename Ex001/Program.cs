13579f243d3ab0ac0a994ffa0954c69ad69bef16
musicboy17 <pratham@yandex.ru>
2022-08-09 04:26:03 +0300

    secondEx

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -5,34 +5,22 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите цифру дня недели - от 1 до 7: ");
+Console.Write("Введите цифру дня недели - от 1 до 7 : ");
+
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number = 1) 
+// Console.WriteLine(number);
+
+if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
 {
     Console.WriteLine("Рабочий день");
 }
-if (number = 2)
-{
-    Console.WriteLine("Рабочий день");
-}
-if (number = 3)
-{
-    Console.WriteLine("Рабочий день");
-}
-if (number = 4)
-{
-    Console.WriteLine("Рабочий день");
-}
-if (number = 5)
-{
-    Console.WriteLine("Рабочий день");
-}
-if (number = 6)
+if (number == 6 || number == 7)
 {
     Console.WriteLine("Неабочий день");
 }
-if (number = 7)
-{
-    Console.WriteLine("Неабочий день");
+if (number != 1 && number !=2 && number != 3 && number != 4 && number != 5 && number != 6 && number != 7) 
+{    
+    Console.WriteLine("Вы ввели некорректное число!");
 }
+
---
+++ Ex002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+//
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number / 10;
+int exit1 = digit % 10;
+Console.WriteLine(exit1);
+
