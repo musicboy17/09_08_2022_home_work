d725a82c5e1f02d9c6dfbab716444c0cce99e2e8
musicboy17 <pratham@yandex.ru>
2022-08-09 03:29:42 +0300

    firstEx

---
+++ Ex001/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру дня недели - от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number = 1) 
+{
+    Console.WriteLine("Рабочий день");
+}
+if (number = 2)
+{
+    Console.WriteLine("Рабочий день");
+}
+if (number = 3)
+{
+    Console.WriteLine("Рабочий день");
+}
+if (number = 4)
+{
+    Console.WriteLine("Рабочий день");
+}
+if (number = 5)
+{
+    Console.WriteLine("Рабочий день");
+}
+if (number = 6)
+{
+    Console.WriteLine("Неабочий день");
+}
+if (number = 7)
+{
+    Console.WriteLine("Неабочий день");
+}
